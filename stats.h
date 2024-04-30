562ab0b91e31f26e45a44552a5dd34a51191f62f
Dan-njuguna <njugush020@gmail.com>
2024-04-30 08:39:46 +0300

    stats.h

--- stats.h
+++ stats.h
@@ -4,10 +4,14 @@
 /*Libraries*/
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*Prototypes*/
 float mean(int *lst, int len);
 int mode(int *lst, int len);
 float median(int *lst, int len);
+float weightedmean(int *lst1, int *lst2,int len);
 
 #endif /*stats.h*/
